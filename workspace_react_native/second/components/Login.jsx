da4d0da8a67169b9ad83f40bea0e9a6edd403fc7
1uuuuuuuuuuuuuuumi <1uuuuuuuuuuuuuuumi@gmail.com>
2025-03-13 12:53:16 +0900

    TextInput, button 디자인, 함수  공부

---
+++ workspace_react_native/second/components/Login.jsx
@@ -0,0 +1,82 @@
+
+import { StyleSheet, Text, TextInput, View } from 'react-native'
+import React, { useState } from 'react'
+import MyTextInput from './MyTextInput'
+import MyButton from './MyButton'
+
+const Login = () => {
+  const [data, setData] = useState({
+    id : '',
+    pw : ''
+  });
+
+  const changeData = (text, name) => {
+    setData({
+      ...data,
+      [name] : text
+      //java를 입력했다면 -> ['id'] : 'java'
+    });
+  }
+
+  // const [id, setId] = useState('');
+  // const [pw, setPw] = useState('');
+
+  const printHello = () => {
+    console.log('안녕하세요');
+  }
+
+  return (
+    <View>
+      <Text style={styles.text}>Login</Text>
+
+      <TextInput 
+        onChangeText={text => changeData(text, 'id')}
+        value={data.id}
+        // value={id}
+      />
+      <TextInput 
+        onChangeText={text => changeData(text, 'pw')}
+        value={data.pw}
+        // value={pw}
+      />
+
+      <MyTextInput 
+        placeholder={'aaa'}
+        onChangeText={text => changeData(text, 'id')}
+        value={data.id}
+      />
+      <MyTextInput 
+        placeholder={'bbb'}
+        onChangeText={text => changeData(text, 'pw')}
+        value={data.pw}
+      />
+
+
+      {/* alert으로 안녕하세요 */}
+      {/* 첫번째 방식 */}
+      <MyButton onPress={() => {alert('안녕하세요')}}/>
+
+      {/* console에 안녕하세요 */}
+      {/* 두번째 방식 - 함수(printHello) */}
+      <MyButton 
+        size='large'
+        onPress={() => {printHello()}}
+      />  
+
+      <MyButton 
+        title='데이터확인' 
+        onPress={() => {
+          alert(`id = ${data.id}, pw = ${data.pw}`)
+        }}/>
+    </View>
+  )
+}
+
+export default Login
+
+const styles = StyleSheet.create({
+  text:{
+    color : '#D31B33',
+    fontSize : 50
+  }
+})
---
+++ workspace_react_native/second/components/MyButton.jsx
@@ -0,0 +1,43 @@
+import { Pressable, StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+import { COLOR } from '../constants/colors'
+
+const MyButton = ({size='normal', onPress, title='버튼'}) => {
+  return (
+    // <Pressable style={[styles.btnContainer, styles[size]]}>  //클래스여러개사용할때의 참고용
+    <Pressable 
+    style={(e) => {
+      return [styles.btnContainer, styles[size], e.pressed && styles.pressed]
+    }}
+    onPress={() => {onPress()}}
+    >
+      <Text style={styles.btn}>{title}</Text>
+    </Pressable>
+  )
+}
+
+export default MyButton
+
+const styles = StyleSheet.create({
+  btnContainer:{
+    height : 30,
+    borderRadius : 6,
+    backgroundColor : COLOR.BTN_BACKGROUND_COLOR,
+    justifyContent : 'center',
+    alignItems : 'center',
+    paddingVertical : 2,    //상하
+    paddingHorizontal : 4   //좌우
+  },
+  btn:{
+    color : '#D31B33'
+  },
+  normal:{
+    width : '30%'
+  },
+  large:{
+    width : '100%'
+  },
+  pressed:{
+    opacity : 0.8
+  }
+})
